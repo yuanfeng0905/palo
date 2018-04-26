c0c613fab65bbd0b900e14bf77ed6e432a0a0172
haituncun <yuanfeng0905@haituncun.com>
2018-04-26 14:01:57 +0800

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,21 @@ set -e
 
 TOP_DIR=$(pwd)/../
 from_source=$1
+ver=$2
+if [ $from_source = "" ] || [ $ver = "" ]; then
+    echo "=== ERR: please attach from_source and version params";
+    exit 1
+fi
+
+if [ $ver = "0.8.0" ]; then
+    PALO_RELEASE_VER="palo-"$ver
+    PALO_RELEASE_URL="http://palo-opensource.gz.bcebos.com/palo-0.8.0-release-20180323.tar.gz?authorization=bce-auth-v1%2F069fc2786e464e63a5f1183824ddb522%2F2018-03-23T07%3A47%3A15Z%2F-1%2Fhost%2Fb3655151fa1f8b52100ea8a987f3de8743a1cdc84e50220482f7e46f6f5ec34c"
+fi
+if [ $ver = "0.8.1" ]; then
+    PALO_RELEASE_VER="palo-"$ver
+    PALO_RELEASE_URL="http://palo-opensource.gz.bcebos.com/palo-0.8.1-release-20180418.tar.gz?authorization=bce-auth-v1%2F069fc2786e464e63a5f1183824ddb522%2F2018-04-18T05%3A11%3A34Z%2F-1%2Fhost%2F9dabe5cb37dd5fa253b41075f360c1c4389fa4965ccd59cb7e774d3c1b099f9f"
+fi
+
 # 如果从源码构建，先下载第三方依赖
 if [ $from_source = "source" ]; then
     echo "=== build from palo source..."
@@ -36,15 +51,12 @@ else
     mkdir -p download
     cd download
     # 直接下载github上release binary file
-    PALO_RELEASE_VER="palo-0.8.1"
     if ! [ -f $PALO_RELEASE_VER.tar.gz ]; then
-        PALO_RELEASE_URL="http://palo-opensource.gz.bcebos.com/palo-0.8.1-release-20180418.tar.gz?authorization=bce-auth-v1%2F069fc2786e464e63a5f1183824ddb522%2F2018-04-18T05%3A11%3A34Z%2F-1%2Fhost%2F9dabe5cb37dd5fa253b41075f360c1c4389fa4965ccd59cb7e774d3c1b099f9f"
         curl -fSL $PALO_RELEASE_URL -o $PALO_RELEASE_VER.tar.gz
     fi
     mkdir -p $PALO_RELEASE_VER
     tar zxvf $PALO_RELEASE_VER.tar.gz -C $PALO_RELEASE_VER --strip-components 1
 fi
-
 
 # 下载jdk1.8支持
 echo "=== download jdk1.8..."
@@ -70,8 +82,8 @@ patch download/$PALO_RELEASE_VER/be/bin/start_be.sh < patchs/start_be.patch
 # 构建docker images
 cd $TOP_DIR/docker
 echo "=== BE image building..."
-docker build -f Dockerfile-be -t palo/palo-be:0.8.1 .
+docker build -f Dockerfile-be -t palo/palo-be:$ver .
 echo "=== ok"
 echo "=== FE image building..."
-docker build -f Dockerfile-fe -t palo/palo-fe:0.8.1 .
+docker build -f Dockerfile-fe -t palo/palo-fe:$ver .
 echo "=== ok"
